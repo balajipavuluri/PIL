9222082cb0601417f0ed81e1cb683e9ab7a67520
balaji <balajipavuluri29@gmail.com>
2021-06-24 21:47:43 +0530

    changes

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-import time
-import streamlit as st
+from time import sleep	
+import streamlit as st	
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-import os
+import os	
+from PIL import Image		
+import sys
+
 
 
 chrome_options = webdriver.ChromeOptions()
@@ -16,99 +19,116 @@ chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
 
-  
-st.title('Nen class join avta')
-with st.form(key="form1"):
-    mail=st.text_input("Enter mail")
-    passw=st.text_input("Enter passw", type="password")
-    #x=st.slider("class no",min_value=1,max_value=4)
-
-    #x=st.text_input("enter class no")
 
 
 
-    x = st.selectbox(
-        'which class  you like to attend?',
-         ('class-1', 'class-2','class-3', 'class-4'))
-
-
-
-    action= st.selectbox(
-        'which   you like to do?',
-         ('listen', 'mute','microphone'))
-
-
-
-    x=list(x)
-    x=x[-1]
-    #st.write(x)
-    y=str(x)
+def get_class_nd_click(x):
+       
+    y=str(x)    
     y='//*[@id="calendar"]/div[2]/div/table/tbody/tr/td/div/div/div[3]/table/tbody/tr/td[2]/div/div[2]/a[{0}]/div'.format(x)
-
- 
-    submit=st.form_submit_button("click here")
-    if submit:
-        
-        driver.maximize_window()
-
-        driver.get("https://rvrjcce.codetantra.com")
-        time.sleep(1)
-        time.sleep(1)
-        l=driver.find_element_by_link_text("Log in")
-        l.click()
-        time.sleep(1)
-
-        
-        m_btn= driver.find_element_by_name('loginId')
-        m_btn.clear()
-        #mail=m+'@gmail.com'
-        m_btn.send_keys(mail)
-        time.sleep(1)
-
-        p_btn=driver.find_element_by_name('password')
-        p_btn.clear()
-        p_btn.send_keys(passw)
-        time.sleep(1)
-
-
-        sub=driver.find_element_by_xpath('//button[text()="Submit"]')
-        sub.click()
-        time.sleep(1)
-        
-
-
-        time.sleep(1)
-        
-        meet_path='/html/body/div[9]/div/div[1]/div/div/div[1]/div/div[2]/a'
-        meet = driver.find_element_by_xpath(meet_path)
-        meet.click()
-
-            
-        time.sleep(1)
-          
-       
-
-        cl= driver.find_element_by_xpath(y) #c_d[x])
-
+    try:
+        sleep(1)
+        cl= driver.find_element_by_xpath(y) #c_d[x])    
         cl.click()
-        try:
-            j=driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div/a')
-            j.click()
+    except:
+        st.write("class link not available")
+        img()
 
-            iframe = driver.find_element_by_xpath("//iframe[@id='frame']")
-            driver.switch_to.frame(iframe)
-            listen = driver.find_elements_by_tag_name("button")
-            for item in listen:
-                attrs = driver.execute_script('var items = {}; for (index = 0; index < arguments[0].attributes.length; ++index) { items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value }; return items;', item)
-            if 'aria-label' in attrs:
-                if attrs['aria-label'] == 'Listen only':
-                    item.click()
-            st.write(f"Class {x} join ayanu friend! Thanks namaste :)")
-        except:
-            st.write(f"Class {x} jaragadam ledu  friend! :(")
-    else:
-        pass
-        time.sleep(1)
-     
+def join_class_nd_sound(x):
+    try:    
+        j=driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div/a')    
+        j.click()   
+        sleep(2)
+        iframe = driver.find_element_by_xpath("//iframe[@id='frame']")  
+        driver.switch_to.frame(iframe)  
+        sleep(2)
+        listen = driver.find_elements_by_tag_name("button") 
+        for item in listen: 
+            attrs = driver.execute_script('var items = {}; for (index = 0; index < arguments[0].attributes.length; ++index) { items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value }; return items;', item)    
+            if 'aria-label' in attrs:   
+                if attrs['aria-label'] == 'Listen only':    
+                    item.click()    
+                    sleep(5)   
+                    img()   
+                    break   
+    except: 
+        img()   
+        st.write("Class not active:",x) 
 
+def start(mail,passw):
+    try:
+                
+        m_btn= driver.find_element_by_name('loginId')   
+        m_btn.clear()   
+        m_btn.send_keys(mail)   
+        sleep(1)   
+        p_btn=driver.find_element_by_name('password')   
+        p_btn.clear()   
+        p_btn.send_keys(passw)  
+        sleep(1)   
+        sub=driver.find_element_by_xpath('//button[text()="Submit"]')   
+        sub.click() 
+        sleep(1)   
+
+           
+        
+        meet_path='/html/body/div[9]/div/div[1]/div/div/div[1]/div/div[2]/a'    
+        meet = driver.find_element_by_xpath(meet_path)  
+        meet.click()    
+
+    except :
+        img()
+        st.write('looks like a error has occured')
        
+
+
+
+def img():	
+    try:
+        driver.save_screenshot("s.jpg")	
+        image = Image.open('s.jpg')	
+        st.image(image, caption='Image of class')	
+        os.remove('s.jpg')	
+    except:
+        img()    
+
+#st.title('bot')	
+with st.form(key="form1"):	
+    mail=st.text_input("Enter mail")	
+    passw=st.text_input("Enter passw", type="password")		
+    #x=st.slider("class no",min_value=1,max_value=4)	
+    class_link = st.selectbox(	
+        'which class  you like to attend?',	
+         ('class-1', 'class-2','class-3', 'class-4'))	
+    
+    	
+ 	
+    submit=st.form_submit_button("click here")
+    if submit:	
+        #driver = webdriver.Chrome('C:/Users/balaji/Desktop/selenium/chromedriver.exe')	
+        driver.maximize_window()	
+        driver.get("https://rvrjcce.codetantra.com")	
+        sleep(2)		
+        l=driver.find_element_by_link_text("Log in")	
+        l.click()	
+        sleep(1)	
+        
+        start(mail,passw)
+
+        sleep(1)
+
+        get_class_nd_click(class_link[-1])
+        sleep(1)
+
+        join_class_nd_sound(class_link[-1])
+        
+
+
+
+        sleep(1)	
+          	
+       	
+
+        
+    else:	
+       pass	
